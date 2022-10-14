3639645680c5c1349084215ebe79c21715b4d3db
izipay-pe <soporteecommerce@izipay.pe>
2022-10-14 15:00:10 -0500

    Redireccion Actualizado

--- RedirectPaymentForm/RedirectPaymentForm/Controllers/HomeController.cs
+++ RedirectPaymentForm/RedirectPaymentForm/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using RedirectPaymentForm.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
@@ -17,35 +18,15 @@ namespace RedirectPaymentForm.Controllers
     {
         public IActionResult Index()
         {
-            ViewBag.vads_action_mode = "INTERACTIVE"; 
-            //MONTO
-            ViewBag.vads_amount = "5000";
-            //MODO TEST O PRODUCCION
-            ViewBag.vads_ctx_mode = "TEST";
-            //CODIGO SOLES=604 DOLARES=840
-            ViewBag.vads_currency = "604";
-            //NÚMERO DE CELULAR
-            ViewBag.vads_cust_cell_phone = "987654321";
-            //CORREO
-            ViewBag.vads_cust_email = "ejemplo@hotmail.com";
-            //NOMBRES
-            ViewBag.vads_cust_first_name = "Nombre";
-            //APELLIDOS
-            ViewBag.vads_cust_last_name = "Apellido";
-            //NÚMERO DE TELEFONO 
-            ViewBag.vads_cust_phone = "5445664";
-            ViewBag.vads_page_action = "PAYMENT";
-            ViewBag.vads_payment_config = "SINGLE";
-            //USUARIO DE BACK OFFICE VENDEDOR (89289758 = usuario de prueba)
-            ViewBag.vads_site_id = "89289758";
-            //CAMBIAR DÍA ACTUAL CON 5 HORAS DE DIFERENCIA CON EL FORMATO  aaaammddhhmmss (202210121550)
-            ViewBag.vads_trans_date = "aaaammddhhmmss";
-            ViewBag.vads_trans_id = "rf54hY";
-            ViewBag.vads_version = "V2";
-            //CONSEGUIR FIRMA DESDE LA WEB https://herramientas-online.com/generador-hmac-online.html 
-            ViewBag.signature = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+            PaymentModel pago = new PaymentModel();
+            pago.vads_action_mode = "INTERACTIVE";
+            pago.vads_page_action = "PAYMENT";
+            pago.vads_payment_config = "SINGLE";
+            pago.vads_version = "V2";
+            pago.KEY = "x6QZKFj2xbklRxek";
 
-            return View();
+
+            return View(pago);
         }  
     }   
 }
